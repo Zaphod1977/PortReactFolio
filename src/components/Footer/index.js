e219be90df1ddfbd395f1e287c9c26fbad9e91f3
carfiguru@hotmail.com <carfiguru@hotmail.com>
2022-09-22 10:41:27 -0500

    adjusted github link on footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer() {
         <div id="con">
             <a>555.555.555</a>
             <a href="https://www.linkedin.com/in/bobby-bridges-baa54889/">Linkedin</a>
-            <a href="https://github.com/Zaphod1977/Professional-Portfolio">Github</a>
+            <a href="https://github.com/Zaphod1977/">Github</a>
             <a href="https://twitter.com/teg_stl">Twitter</a>
             <a href="https://open.spotify.com/show/2ip0Ojn7AwzukeawerfPDE">Spotify</a>
         </div>
