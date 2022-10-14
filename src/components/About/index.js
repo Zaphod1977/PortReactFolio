60b90c2af17eff158b385d20273d3d5a9ad55a56
carfiguru@hotmail.com <carfiguru@hotmail.com>
2022-10-13 19:31:42 -0500

    modest naming changes

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,7 @@ function About() {
     <section id="about" className="aboutme">
       <h2 id="about" className="aboutme-leftside">About</h2>
       <p className="aboutme-rightside">
-            My name is Zaphod Bobbybrox. I am a 45 year old father, business owner, and now full stack developer. I have
+            My name is Bobby Bridges. I am a 45 year old father, business owner, and now full stack developer. I have
             been in the tech world since I was a kid taking
             audio gear apart to figure out how it worked and how to make it do different things I wanted to make it do.
             That enginuity brought me to the audio/video and automation
